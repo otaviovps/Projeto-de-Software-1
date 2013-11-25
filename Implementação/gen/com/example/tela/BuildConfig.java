931b8ea8820e759c705ba8c7d6724e80bd09baab
otaviovps <otaviovpinheiro@gmail.com>
2013-11-25 18:21:54 -0300

    Correção do commit (código)

---
+++ Implementação/gen/com/example/tela/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.tela;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Implementação/src/com/example/tela/TelaLogin.java
@@ -0,0 +1,23 @@
+package com.example.tela;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TelaLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tela_login, menu);
+        return true;
+    }
+    
+}
